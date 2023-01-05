693ca141a1752f9238ec3a17b666d0fbb0a6a037
Tarek Habash <tarek.soliman797@gmail.com>
2023-01-05 21:45:55 +0100

    pass method references around

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,36 +1,43 @@
-class Tooltip {}
+class Tooltip { }
 
 class ProjectItem {
-    constructor(id){
+    constructor(id, updateProjectListsFunction) {
         this.id = id
+        this.updateProjectListsHandler = updateProjectListsFunction
         this.connectMoreInfoButton()
         this.connectSwitchButton()
     }
-    connectMoreInfoButton(){
+    connectMoreInfoButton() {
 
     }
-    connectSwitchButton(){
+    connectSwitchButton() {
         const projectItemElement = document.getElementById(this.id)
         const switchBtn = projectItemElement.querySelector('button:last-of-type')
-        switchBtn.addEventListener('click', )
+        switchBtn.addEventListener('click', this.updateProjectListsHandler)
     }
 }
 
 class ProjectList {
     projects = [];
 
-    constructor(type){
+    constructor(type) {
         const prjItems = document.querySelectorAll(`#${type}-projects li`)
         console.log(prjItems)
-        for (const prjItem of prjItems){
-            this.projects.push(new ProjectItem(prjItem.id));
+        for (const prjItem of prjItems) {
+            this.projects.push(new ProjectItem(prjItem.id, this.switchProject.bind(this)));
         }
     }
-    addProject(){
 
+    setSwitchHandlerFunction(switchHandlerFunction) {
+        this.switchHandler = switchHandlerFunction
+    }
+    addProject() {
+        console.log("finished")
     }
     switchProject(projectId) {
         //this.addProject(); it's not logically correct. we should add the project to other instance.
+
+        this.switchHandler(this.projects.find(p => p.id === projectId))
         this.projects = this.projects.filter(p => p.id !== projectId)
     }
 }
@@ -39,6 +46,9 @@ class App {
     static init() {
         const activeProjectList = new ProjectList("active")
         const finishedProjectList = new ProjectList("finished")
+        activeProjectList.setSwitchHandlerFunction(finishedProjectList.addProject.bind(finishedProjectList))
+        finishedProjectList.setSwitchHandlerFunction(activeProjectList.addProject.bind(activeProjectList))
+
     }
 }
 
