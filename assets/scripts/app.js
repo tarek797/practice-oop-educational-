e5ffeeb499c7d46df9ae2badcbf6229023774915
Tarek Habash <tarek.soliman797@gmail.com>
2023-01-05 20:16:45 +0100

    create project list and  parse element data

---
+++ assets/scripts/app.js
@@ -0,0 +1,19 @@
+class Tooltip {}
+
+class ProjectItem {}
+
+class ProjectList {
+    constructor(type){
+        const prjItems = document.querySelectorAll(`#${type}-projects li`)
+        console.log(prjItems)
+    }
+}
+
+class App {
+    static init() {
+        const activeProjectList = new ProjectList("active")
+        const finishedProjectList = new ProjectList("finished")
+    }
+}
+
+App.init();
