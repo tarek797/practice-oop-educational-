0c53c999bf74de3f859b77cb40b2aeb5984e48d9
Tarek Habash <tarek.soliman797@gmail.com>
2023-01-06 00:09:53 +0100

    move DOM elements

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,19 +1,42 @@
+class DOMHelper {
+    static clearEventListeners(element) {
+        const clonedElement = element.cloneNode(true)
+        element.replaceWith(clonedElement)
+        return clonedElement
+    }
+
+    static moveElement(elementId, newDestinationSelector){
+        const element = document.getElementById(elementId)
+        const destinationElement = document.querySelector(newDestinationSelector)
+        destinationElement.append(element)
+    }
+
+}
+
 class Tooltip { }
 
 class ProjectItem {
-    constructor(id, updateProjectListsFunction) {
+    constructor(id, updateProjectListsFunction, type) {
         this.id = id
         this.updateProjectListsHandler = updateProjectListsFunction
         this.connectMoreInfoButton()
-        this.connectSwitchButton()
+        this.connectSwitchButton(type)
     }
     connectMoreInfoButton() {
 
     }
-    connectSwitchButton() {
+    connectSwitchButton(type) {
         const projectItemElement = document.getElementById(this.id)
-        const switchBtn = projectItemElement.querySelector('button:last-of-type')
-        switchBtn.addEventListener('click', this.updateProjectListsHandler)
+        let switchBtn = projectItemElement.querySelector('button:last-of-type')
+        switchBtn = DOMHelper.clearEventListeners(switchBtn)
+        switchBtn.textContent = type === 'active' ? 'Finish' : 'Activate';
+        switchBtn.addEventListener('click', this.updateProjectListsHandler.bind(null, this.id))
+
+    }
+
+    update(updateProjectListsFn, type){
+        this.updateProjectListsHandler = updateProjectListsFn
+        this.connectSwitchButton(type)
     }
 }
 
@@ -21,22 +44,23 @@ class ProjectList {
     projects = [];
 
     constructor(type) {
+        this.type = type
         const prjItems = document.querySelectorAll(`#${type}-projects li`)
-        console.log(prjItems)
         for (const prjItem of prjItems) {
-            this.projects.push(new ProjectItem(prjItem.id, this.switchProject.bind(this)));
+            this.projects.push(new ProjectItem(prjItem.id, this.switchProject.bind(this), this.type));
         }
     }
 
     setSwitchHandlerFunction(switchHandlerFunction) {
         this.switchHandler = switchHandlerFunction
     }
-    addProject() {
-        console.log("finished")
+    addProject(project) {
+        this.projects.push(project)
+        DOMHelper.moveElement(project.id, `#${this.type}-projects ul`)
+        project.update(this.switchProject.bind(this), this.type)
     }
     switchProject(projectId) {
         //this.addProject(); it's not logically correct. we should add the project to other instance.
-
         this.switchHandler(this.projects.find(p => p.id === projectId))
         this.projects = this.projects.filter(p => p.id !== projectId)
     }
